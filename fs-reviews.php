2c5b2bdcca75d338505f09a08cc50a6e7e5c6502
Shramee Srivastav <shramee.srivastav@gmail.com>
2017-09-27 19:43:51 +0530

    Plugin header

---
+++ fs-reviews.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Freemius Reviews
+Description: Shows plugins/theme reviews from Freemius
+Version: 1.0
+Author: Shramee Srivastav
+Author URI: http://shramee.com
+Author Email: shramee.srivastav@gmail.com
+*/
